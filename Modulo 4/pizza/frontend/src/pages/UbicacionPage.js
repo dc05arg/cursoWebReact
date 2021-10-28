687b1b7935c5364191cabb20c4d7cafd30506736
Diego Cavalli <dhcavalli@hotmail.com>
2021-10-28 16:02:17 -0300

    "se borran cosas del index.html y dos jpf react, se cambia título página, se modifican archivos css y js "

--- Modulo 4/pizza/frontend/src/pages/UbicacionPage.js
+++ Modulo 4/pizza/frontend/src/pages/UbicacionPage.js
@@ -13,7 +13,7 @@ const UbicacionPage = (props) => {
             </div>
             <div className="mapagoogle">
                 <iframe title="ubicacion" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52502.855079198634!2d-58.651703300805856!3d-34.6691347508206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc76f57e3be01%3A0xa488fcdcec5b600f!2sMor%C3%B3n%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1629843903005!5m2!1ses-419!2sar"></iframe>
-                 {/* El estilo inicial para css width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"  */}
+                
             </div>
         </main>
     );
--- Modulo 4/pizza/frontend/src/pages/contactoPage.js
+++ Modulo 4/pizza/frontend/src/pages/contactoPage.js
@@ -2,35 +2,37 @@ import '../styles/components/pages/contactoPage.css';
 const contactoPage = (props) => {
     return (
         <main className="holder">
-            <div className="columna left">
-                <h2>Formulario de Contacto y Pedido</h2>
-                <form action="" method="" className="formulario">
-                    <p>
-                        <label>Nombre</label>
-                        <input type="text"/>
-                    </p>
-                    <p>
-                        <label>Apellido</label>
-                        <input type="text"/>  
-                    </p>
-                    <p>
-                        <label>Dirección</label>
-                        <input type="text"/>  
-                    </p>
-                    <p>
-                        <label>Localidad</label>
-                        <input type="text"/>  
-                    </p>
-                    <p>
-                        <label>Teléfono</label>
-                        <input type="text"/>  
-                    </p>
-                    <p>
-                        <label>Pedido</label>
-                        <textarea id="pedido"></textarea>
-                    </p>
-                    <p className="acciones"><input type="submit" value="Enviar"/></p>
-                </form>
+            <div className="contenedor">
+                <div className="columna left">
+                    <h2>Formulario de Contacto y Pedido</h2>
+                    <form action="" method="" className="formulario">
+                        <p>
+                            <label>Nombre</label>
+                            <input type="text"/>
+                        </p>
+                        <p>
+                            <label>Apellido</label>
+                            <input type="text"/>  
+                        </p>
+                        <p>
+                            <label>Dirección</label>
+                            <input type="text"/>  
+                        </p>
+                        <p>
+                            <label>Localidad</label>
+                            <input type="text"/>  
+                        </p>
+                        <p>
+                            <label>Teléfono</label>
+                            <input type="text"/>  
+                        </p>
+                        <p>
+                            <label>Pedido</label>
+                            <textarea id="pedido"></textarea>
+                        </p>
+                        <p className="acciones"><input type="submit" value="Enviar"/></p>
+                    </form>
+                </div>
             </div>
             <div className="columna right">
                 <h2>Otras vías de contacto</h2>
