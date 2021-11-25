f05218de22f8f802861cd37fe99948effe78e57c
Diego Cavalli <dhcavalli@hotmail.com>
2021-11-25 10:29:04 -0300

    "se modifica contactoPage.js y api.js para formulario envio"

--- Modulo 4/pizza/backend/routes/api.js
+++ Modulo 4/pizza/backend/routes/api.js
@@ -2,21 +2,22 @@ var express = require('express');
 var router = express.Router();
 var menuModel = require('./../models/menuModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
 router.get('/menu', async function (req, res, next) {
     let menu = await menuModel.getMenu();
 
     menu = menu.map(menu => {
         if (menu.img_id) {
-        const imagen = cloudinary.image(menu.img_id, {
-            width: 960,
-            height: 200,
-            crop: 'fill'
-        });
-        return {
-            ...menu,
-            imagen
-        }
+            const imagen = cloudinary.image(menu.img_id, {
+                width: 960,
+                height: 200,
+                crop: 'fill'
+            });
+            return {
+                ...menu,
+                imagen
+            }
         } else {
             return {
                 ...menu,
@@ -24,8 +25,39 @@ router.get('/menu', async function (req, res, next) {
             }
         }
     });
-    
+
     res.json(menu);
+}); //termina menú
+
+//inicio contacto
+
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'cavallidh@gmail.com',
+        subjet: 'Contacto web',
+        html: `${req.body.nombre} ${req.body.apellido}  se contacto a través de la web. <br>
+        Su dirección de envío es: ${req.body.direccion} <br>
+        Su localidad es: ${req.body.localidad} <br>
+        Su teléfono es: ${req.body.telefono} <br>
+        Su correo electrónico es: ${req.body.email} <br>
+        Su pedido es: ${req.body.pedido} <br>` //modificar para la pizza
+    }
+    
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    }); //cierra transport
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    });
 });
 
 module.exports = router;
--- Modulo 4/pizza/frontend/src/pages/contactoPage.js
+++ Modulo 4/pizza/frontend/src/pages/contactoPage.js
@@ -1,35 +1,79 @@
 import '../styles/components/pages/contactoPage.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 const contactoPage = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        apellido: '',
+        direccion: '',
+        localidad: '',
+        telefono: '',
+        email: '',
+        pedido:''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            oldData,
+            [name]: value //forma dinámica
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
     return (
         <main className="holder">
             <div className="contenedor">
                 <div className="columna left">
                     <h2>Formulario de Contacto y Pedido</h2>
-                    <form action="" method="" className="formulario">
+                    <form action="/contacto" method="post" className="formulario" onSubmit={handleSubmit}>
                         <p>
                             <label>Nombre</label>
-                            <input type="text"/>
+                            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
                         </p>
                         <p>
                             <label>Apellido</label>
-                            <input type="text"/>  
+                            <input type="text"name="apellido" value={formData.apellido} onChange={handleChange}/>  
                         </p>
                         <p>
                             <label>Dirección</label>
-                            <input type="text"/>  
+                            <input type="text" name="direccion" value={formData.direccion} onChange={handleChange}/>  
                         </p>
                         <p>
                             <label>Localidad</label>
-                            <input type="text"/>  
+                            <input type="text" name="localidad" value={formData.localidad} onChange={handleChange}/>  
                         </p>
                         <p>
                             <label>Teléfono</label>
-                            <input type="text"/>  
+                            <input type="text" name="telefono" value={formData.telefono} onChange={handleChange}/>  
+                        </p>
+                        <p>
+                            <label>Email</label>
+                            <input type="text" name="email" value={formData.email} onChange={handleChange}/>  
                         </p>
                         <p>
                             <label>Pedido</label>
-                            <textarea id="pedido"></textarea>
+                            <textarea id="pedido" name="pedido" value={formData.pedido} onChange={handleChange}></textarea>
                         </p>
+                        {sending ? <p>Enviando...</p> : null}
+                        {msg ? <p>{msg}</p> : null}
                         <p className="acciones"><input type="submit" value="Enviar"/></p>
                     </form>
                 </div>
