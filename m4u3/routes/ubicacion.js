c6f21e9cddb0d8bd4c7a189e6beb426027613df2
dc05arg <68360252+dc05arg@users.noreply.github.com>
2022-01-03 23:14:22 -0300

    Delete m4u3 directory

--- m4u3/routes/ubicacion.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET ubicación page. */
-router.get('/', function(req, res, next) {
-  res.render('ubicacion');
-});
-
-module.exports = router;
