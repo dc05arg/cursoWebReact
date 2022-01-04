f301630c5804f75df317fc679f1421347db6edb2
dc05arg <68360252+dc05arg@users.noreply.github.com>
2022-01-03 23:13:48 -0300

    Delete Modulo 3/Unidad 4 Javascript (parte2) directory

--- Modulo 3/Unidad 4 Javascript (parte2)/contador.js
+++
@@ -1,9 +0,0 @@
-function contador(){
-    var cantidad = document.getElementById("text").value;
-    var contador = cantidad.length;
-    
-    console.log("Escribistes " + contador + " caracteres!");
-    
-    document.getElementById("total").innerText = "Escribistes " + contador + " caracteres";
-
-    }
